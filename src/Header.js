3847d53c7847c627308da432fe4c64fd1f42f14d
jacerchetoui56 <77737324+jacerchetoui56@users.noreply.github.com>
2022-06-30 14:55:32 +0100

    fixing the naming and adding some style

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className='header'>
             <div>
-                Food Menu
+                Our Tours
             </div>
         </header>
     )
